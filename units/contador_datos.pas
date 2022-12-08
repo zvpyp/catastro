27d6bbb8d3b41b78f60bd0deac101f82d925f9a4
Máximo Fernández <maximo03f@gmail.com>
2022-12-07 22:00:37 -0300

    Añadido contador_datos.pas

---
+++ units/contador_datos.pas
@@ -0,0 +1,110 @@
+unit contador_datos;
+
+{ Unidad para conteo de contribuyentes y terrenos guardados.
+  Nota: se utiliza esta unit para evitar leer posiciones vacías. }
+
+{--------------------------------}
+
+interface
+    const
+        ruta_archivo_contador = './contador_datos.dat'
+
+    type
+    t_contador_datos = record
+        contribuyentes : cardinal;
+        terrenos : cardinal;
+        end;
+    
+    // NOTA: solamente trabajar con posición 1.
+    t_archivo_contador = file of t_contador_datos;
+
+    // Abre el archivo contador. Si no existe, lo crea y se setea en default.
+    procedure abrir_archivo_contador(var archivo : t_archivo_contador);
+
+    // Syntatic sugar. 
+    procedure cerrar_archivo_contador(var archivo : t_archivo_contador);
+
+    // Suma un contribuyente a los datos.
+    procedure contar_contribuyente(var archivo : t_archivo_contador);
+
+    // Suma un terreno a los datos.
+    procedure contar_terreno(var archivo : t_archivo_contador);
+
+
+{--------------------------------}
+
+implementation
+
+    procedure abrir_archivo_contador(var archivo : t_archivo_contador);
+    var
+        contador_default : t_contador_datos;
+    begin
+        assign(archivo, ruta_archivo_contador);
+        
+        {$I-} reset(archivo) {$I+};
+
+        // Si el archivo no existe, lo crea.
+        if ioresult <> 0 then
+        begin
+            rewrite(archivo);
+
+            contador_default.contribuyentes := 0;
+            contador_default.terrenos := 0;
+
+            seek(archivo, 1);
+            write(archivo, contador_default);
+        end;
+    end;
+
+    procedure cerrar_archivo_contador(var archivo : t_archivo_contador);
+    begin
+        close(archivo);
+    end;
+
+    procedure contar_contribuyente(var archivo : t_archivo_contador);
+    var
+        contador_aux : t_contador_datos;
+    begin
+        seek(archivo, 1);
+        read(archivo, contador_aux);
+
+        contador_aux.contribuyentes := contador_aux.contribuyentes + 1;
+
+        seek(archivo, 1);
+        write(archivo, contador_aux);
+    end;
+
+    procedure contar_terreno(var archivo : t_archivo_contador);
+    var
+        contador_aux : t_contador_datos;
+    begin
+        seek(archivo, 1);
+        read(archivo, contador_aux);
+
+        contador_aux.terrenos := contador_aux.terrenos + 1;
+
+        seek(archivo, 1);
+        write(archivo, contador_aux);
+    end;
+
+    function cantidad_contribuyentes(var archivo_ t_archivo_contador): cardinal;
+    var
+        contador_aux : t_contador_datos;
+    begin
+        seek(archivo, 1);
+        read(archivo, contador_aux);
+
+        cantidad_contribuyentes := contador_aux.contribuyentes;
+    end;
+
+    function cantidad_terrenos(var archivo_ t_archivo_contador): cardinal;
+    var
+        contador_aux : t_contador_datos;
+    begin
+        seek(archivo, 1);
+        read(archivo, contador_aux);
+
+        cantidad_terrenos := contador_aux.terrenos;
+    end;
+    
+end.
