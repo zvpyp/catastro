47283b6fb7e3642668df954f7a7395ec5fd86778
mateo <mateoedgardogomez@gmail.com>
2022-12-15 21:32:46 -0300

    Arboles terrenos por fecha y por nro de contribuyente

--- units/arbol/arbol_terrenos.pas
+++ units/arbol/arbol_terrenos.pas
@@ -13,7 +13,8 @@ Unit arbol_terrenos;
 Interface
 
 Uses terreno in './units/terreno.pas',
-arbol in 'units/arbol/arbol';
+arbol in 'units/arbol/arbol.pas',
+compara_fechas in 'units/compara_fechas.pas';
 
 // Retorna un árbol binario ordenado por numero de contribuyente asociado
 // a cada terreno.
@@ -24,11 +25,6 @@ Function arbol_ordenado_por_nro_contribuyente(Var archivo : t_archivo_terrenos;
 // Retorna un árbol binario ordenado por fecha de inscripcion de cada terreno.
 Function arbol_ordenado_por_fecha_inscripcion(Var archivo : t_archivo_terrenos;
                                               cantidad_terrenos : cardinal): t_arbol
-;
-
-// Retorna un árbol binario ordenado por zona de cada terreno.
-Function arbol_ordenado_por_zona(Var archivo : t_archivo_terrenos;
-                                 cantidad_terrenos : cardinal): t_arbol
 ;
 
 {--------------------------------}
@@ -79,48 +75,6 @@ Begin
     End;
 End;
 
-// Agrega un nodo a un arbol ordenado por zona.
-// Su raíz será la posición en el archivo dado.
-// El valor se determina según el zona del terreno dado.
-Procedure sumar_por_zona(Var arbol: t_arbol;
-                        Var archivo : t_archivo_terrenos;
-                        nuevo_terreno_indice : cardinal);
-
-Var 
-  nuevo_terreno : t_terreno;
-  zona_actual : string;
-Begin
-  _actual := arbol.clave;
-
-  // lee el terreno a agregar
-  seek(archivo, nuevo_terreno_indice);
-  read(archivo, nuevo_terreno);
-
-  // Caso en que el zona sea mayor:
-  If (zona_actual < nuevo_terreno.zona) Then
-    Begin
-
- // Verifica si el árbol tiene hijo derecho y repite el proceso recursivamente.
-      // Sino, lo agrega como hijo derecho.
-      If tiene_hijo_der(arbol) Then
-        sumar_por_zona(arbol.sd^, archivo, nuevo_terreno_indice)
-      Else
-        anidar_hijo_der(arbol, nuevo_terreno_indice, nuevo_terreno.zona);
-    End
-    // Caso en que sea menor
-  Else
-    Begin
-
-// Verifica si el árbol tiene hijo izquierdo y repite el proceso recursivamente.
-      // Sino, lo agrega como hijo izquierdo.
-      If tiene_hijo_izq(arbol) Then
-        sumar_por_zona(arbol.si^, archivo, nuevo_terreno_indice)
-      Else
-        anidar_hijo_izq(arbol, nuevo_terreno_indice, nuevo_terreno.zona);
-    End;
-End;
-
-
 Function arbol_ordenado_por_nro_contribuyente(Var archivo : t_archivo_terrenos;
                                     cantidad_terrenos : cardinal): t_arbol;
 
@@ -144,22 +98,70 @@ Begin
     End;
 End;
 
-Function arbol_ordenado_por_zona(Var archivo : t_archivo_terrenos;
-                                cantidad_terrenos : cardinal): t_arbol;
+// Agrega un nodo a un arbol ordenado por fecha de inscripción.
+// Su raíz será la posición en el archivo dado.
+// El valor se determina según la fecha de inscripción del terreno dado.
+// Aclaración: se utiliza la función fecha_es_mayor para realizar la comparativa.
+Procedure sumar_por_fecha_inscripcion(Var arbol: t_arbol;
+                           Var archivo : t_archivo_terrenos;
+                           nuevo_terreno_indice : cardinal);
 
+Var 
+  nuevo_terreno : t_terreno;
+  fecha_inscripcion_nuevo : string;
+  fecha_inscripcion_actual : string;
+Begin
+
+  // lee el terreno a agregar
+  seek(archivo, nuevo_terreno_indice);
+  read(archivo, nuevo_terreno);
+
+  fecha_inscripcion_actual := arbol.clave;
+  fecha_inscripcion_nuevo := nuevo_terreno.fecha_inscripcion;
+
+  // Caso en que la fecha de inscripcion sea mayor:
+  If fecha_es_mayor(fecha_inscripcion_nuevo, fecha_inscripcion_actual) Then
+    Begin
+
+ // Verifica si el árbol tiene hijo derecho y repite el proceso recursivamente.
+      // Sino, lo agrega como hijo derecho.
+      If tiene_hijo_der(arbol) Then
+        sumar_por_fecha_inscripcion(arbol.sd^, archivo, nuevo_terreno_indice)
+      Else
+        anidar_hijo_der(arbol, nuevo_terreno_indice, fecha_inscripcion_nuevo);
+    End
+    // Caso en que sea menor
+  Else
+    Begin
+
+// Verifica si el árbol tiene hijo izquierdo y repite el proceso recursivamente.
+      // Sino, lo agrega como hijo izquierdo.
+      If tiene_hijo_izq(arbol) Then
+        sumar_por_fecha_inscripcion(arbol.si^, archivo, nuevo_terreno_indice)
+      Else
+        anidar_hijo_izq(arbol, nuevo_terreno_indice, fecha_inscripcion_nuevo);
+    End;
+End;
+
+Function arbol_ordenado_por_fecha_inscripcion(Var archivo : t_archivo_terrenos;
+                                              cantidad_terrenos : cardinal): t_arbol
 Var 
   indice_actual : cardinal;
   primer_terreno : t_terreno;
+  fecha_inscripcion_primero : string;
 Begin
   // Creamos el árbol con el primer índice.
   indice_actual := 1;
   seek(archivo, indice_actual);
   read(archivo, primer_terreno);
-  arbol_ordenado_por_zona := crear_arbol(indice_actual, primer_terreno.zona);
+  fecha_inscripcion_primero := primer_terreno.fecha_inscripcion;
+
+  arbol_ordenado_por_fecha_inscripcion := crear_arbol(indice_actual,
+                                fecha_inscripcion_primero);
 
   For indice_actual := 2 To cantidad_terrenos Do
     Begin
-      sumar_por_zona(arbol_ordenado_por_zona, archivo, indice_actual);
+      sumar_por_fecha_inscripcion(arbol_ordenado_por_fecha_inscripcion, archivo, indice_actual);
     End;
 End;
 
