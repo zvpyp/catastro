0a0fa0d662c09dd868b3d5e8d77214f7fbafc302
Máximo Fernández <maximo03f@gmail.com>
2022-12-17 22:17:58 -0300

    ahora sí, añadidos comentarios útiles kkkk

--- units/usuario/usuario_listados_estadisticas.pas
+++ units/usuario/usuario_listados_estadisticas.pas
@@ -11,9 +11,18 @@ interface
 implementation
 
 // TODO:
+// AÑADIR RECORRIDO PREORDEN SOBRE EL ARBOL DE CONTRIBUYENTES:
 // procedure listado_contribuyentes_propiedades();
+
+// HACER RECORRIDO EN LISTA POR FECHAS JEJEJE
 // procedure listado_fecha_inscripciones();
+
+// PRIMERO HAY QUE CREAR UN ARRAY DE TERRENOS
+// QUE CONTENGA UN RECORD DE ESTILO ZONA1, ZONA2, ZONA3...
+// LUEGO RECORRERLO Y LISTO (ENTENDISTE EL CHISTE?¿¿¿!¿)
 // procedure listado_zona_terrenos();
+
+// DESPUÉS DE TODAS, ESTA ES LA MÁS FÁCIL XD.
 // procedure mostrar_estadisticas();
 
 end.
