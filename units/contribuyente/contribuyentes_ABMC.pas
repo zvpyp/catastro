009998411e9684087946c2f3602b4a702880ab6c
mateo <mateoedgardogomez@gmail.com>
2022-12-20 08:58:33 -0300

    ABMC Contribuyentes 100% terminada

--- units/contribuyente/contribuyentes_ABMC.pas
+++ units/contribuyente/contribuyentes_ABMC.pas
@@ -10,40 +10,67 @@ Uses contribuyente in 'units/contribuyente/contribuyente.pas',
      validacion_entradas in 'units/varios/validacion_entradas.pas',
      contador_datos in 'units/varios/contador_datos.pas',
      arbol in 'units/arbol/arbol.pas',
+     arbol_contribuyentes in 'units/arbol/arbol_contribuyentes.pas',
      crt, sysutils;
 
-// Pasar archivo de contribuyentes y arbol ordenado por nro de contribuyente.
-Procedure alta_contribuyente(var archivo : t_archivo_contribuyentes; var arbol : t_arbol; var archivo_contador : t_archivo_contador);
+// Pasar archivo de contribuyentes y arbol ordenado por nro de contribuyente, nombre y dni.
+Procedure alta_contribuyente(var archivo_contribuyentes : t_archivo_contribuyentes;
+                             var arbol_nro : t_arbol;
+                             var arbol_nombre : t_arbol;
+                             var arbol_dni : t_arbol;
+                             var archivo_contador : t_archivo_contador);
 
-// Pasar archivo de contribuyentes y arbol ordenado por nro de contribuyente.
-Procedure baja_contribuyente(var archivo : t_archivo_contribuyentes; var arbol : t_arbol);
+// Pasar archivo de contribuyentes y arbol ordenado por nro de contribuyente, nombre y dni.
+Procedure baja_contribuyente(var archivo : t_archivo_contribuyentes;
+                             var arbol_nro : t_arbol;
+                             var arbol_nombre : t_arbol;
+                             var arbol_dni : t_arbol);
 
-Procedure mod_contribuyente(var archivo : t_archivo_contribuyentes; var arbol : t_arbol);
+Procedure mod_contribuyente(var archivo : t_archivo_contribuyentes;
+                            var arbol_nro : t_arbol;
+                            var arbol_nombre : t_arbol;
+                            var arbol_dni : t_arbol);
 
-Procedure consulta_contribuyente(var archivo : t_archivo_contribuyentes; var arbol_nombre : t_arbol; var arbol_numero : t_arbol);
+Procedure consulta_contribuyente(var archivo : t_archivo_contribuyentes;
+                                 var arbol_nro : t_arbol;
+                                 var arbol_nombre : t_arbol);
 
 Implementation
 
-// Agregar que se modifique el arbol.
-Procedure alta_contribuyente(var archivo : t_archivo_contribuyentes; var arbol : t_arbol; var archivo_contador : t_archivo_contador);
+Procedure alta_contribuyente(var archivo_contribuyentes : t_archivo_contribuyentes;
+                             var arbol_nro : t_arbol;
+                             var arbol_nombre : t_arbol;
+                             var arbol_dni : t_arbol;
+                             var archivo_contador : t_archivo_contador);
 var
 contribuyente_nuevo : t_contribuyente;
+indice : cardinal;
 begin
-    crear_contribuyente(archivo, arbol, contribuyente_nuevo);
+    indice := cantidad_contribuyentes(archivo_contador) + 1;
+    crear_contribuyente(archivo_contribuyentes, arbol_nro, contribuyente_nuevo);
 
     if (contribuyente_nuevo.numero <> '') then
     begin
-      escribir_contribuyente(archivo, contribuyente_nuevo, cantidad_contribuyentes(archivo_contador) + 1);
+      // Guarda el contribuyente en el archivo.
+      escribir_contribuyente(archivo, contribuyente_nuevo, indice);
+      // Modifica todos los árboles.
+      sumar_por_nro(arbol_nro, archivo, indice);
+      sumar_por_nombre(arbol_nombre, archivo, indice);
+      sumar_por_dni(arbol_dni, archivo, indice);
+      // Aumenta la cantidad de contribuyentes.
       contar_contribuyente(archivo);
     end;
 end;
 
-Procedure baja_contribuyente(var archivo : t_archivo_contribuyentes; var arbol : t_arbol);
+Procedure baja_contribuyente(var archivo : t_archivo_contribuyentes;
+                             var arbol_nro : t_arbol;
+                             var arbol_nombre : t_arbol;
+                             var arbol_dni : t_arbol);
 var
-contribuyente_baja : t_contribuyente;
-nro_contribuyente_baja : string;
-arbol_pos : t_arbol;
-pos, tcl : int16;
+  contribuyente_baja : t_contribuyente;
+  nro_contribuyente_baja, clave_nombre, clave_dni : string;
+  arbol_nro_baja, arbol_nombre_baja, arbol_dni_baja  : t_arbol;
+  pos, tcl : int16;
 begin
 
   Writeln('Introduzca el número de contribuyente del usuario que desea dar de baja: ');
@@ -53,8 +80,8 @@ begin
         Writeln('El valor ingresado no es un número o supera los 15 caracteres, ingréselo nuevamente por favor');
         Readln(nro_contribuyente_baja);
     end;
-    arbol_pos := buscar_por_clave(arbol, nro_contribuyente_baja);
-    pos := arbol_pos.indice;
+    arbol_nro_baja := buscar_por_clave(arbol_nro, nro_contribuyente_baja);
+    pos := arbol_nro_baja.indice;
     // Buscamos si el numero existe ya en el archivo.
     While (pos = 0) and (tcl <> -1) do
         begin
@@ -66,23 +93,37 @@ begin
             1: Readln(nro_contribuyente_baja);
             2: tcl := -1;
             end;
-            arbol_pos := buscar_por_clave(arbol, nro_contribuyente_baja);
-            pos := arbol_pos.indice;
+            arbol_nro_baja := buscar_por_clave(arbol_nro, nro_contribuyente_baja);
+            pos := arbol_nro_baja.indice;
         end;
-          if tcl <> -1 then
-            begin
-                contribuyente_baja := leer_contribuyente(archivo, pos);
-                borrar_contribuyente(contribuyente_baja);
-                escribir_contribuyente(archivo, contribuyente_baja, pos);
+    if tcl <> -1 then
+      begin
+        // Modificamos el archivo.
+        contribuyente_baja := leer_contribuyente(archivo, pos);
+        borrar_contribuyente(contribuyente_baja);
+        escribir_contribuyente(archivo, contribuyente_baja, pos);
+        // Buscamos las claves de los árboles.
+       clave_nombre := contribuyente_baja.apellido + ' ' + contribuyente_baja.nombre;
+       clave_dni := contribuyente_baja.dni;
+       // Buscamos los árboles.
+       arbol_nombre_baja := buscar_por_clave(arbol_nombre, clave_nombre);
+       arbol_dni_baja := buscar_por_clave(arbol_dni, clave_dni);
+       // Modificamos los árboles.
+       arbol_nro_baja.estado := false;
+       arbol_nombre_baja.estado := false;
+       arbol_dni_baja.estado := false;
             end;
 end;
 
-Procedure mod_contribuyente(var archivo : t_archivo_contribuyentes; var arbol : t_arbol);
+Procedure mod_contribuyente(var archivo : t_archivo_contribuyentes;
+                            var arbol_nro : t_arbol;
+                            var arbol_nombre : t_arbol;
+                            var arbol_dni : t_arbol);
 var
-contribuyente_modificado : t_contribuyente;
-nro_contribuyente_modificado : string;
-arbol_pos : t_arbol;
-pos, tcl : int16;
+  contribuyente_modificado : t_contribuyente;
+  nro_contribuyente_modificado, clave_nombre, clave_dni : string;
+  arbol_nro_mod, arbol_nombre_mod, arbol_dni_mod : t_arbol;
+  pos, tcl : int16;
 begin
 
   Writeln('Introduzca el número de contribuyente del usuario que desea modificar: ');
@@ -92,39 +133,62 @@ begin
         Writeln('El valor ingresado no es un número o supera los 15 caracteres, ingréselo nuevamente por favor');
         Readln(nro_contribuyente_modificado);
     end;
-    arbol_pos := buscar_por_clave(arbol, nro_contribuyente_modificado);
-    pos := arbol_pos.indice;
-    // Buscamos si el numero existe ya en el archivo.
-    While (pos = 0) and (tcl <> -1) do
-        begin
-            Writeln('No existe ningún usuario con este número de contribuyente, que desea hacer?');
-            Writeln('1. Ingresar otro número de contribuyente');
-            Writeln('2. Regresar a la pantalla anterior');
-            Readln(tcl);
-            Case tcl of
-            1: Readln(nro_contribuyente_modificado);
-            2: tcl := -1;
-            end;
-            arbol_pos := buscar_por_clave(arbol, nro_contribuyente_modificado);
-            pos := arbol_pos.indice;
-        end;
-          if tcl <> -1 then
-            begin
-                contribuyente_modificado := leer_contribuyente(archivo, pos);
-                modificar_contribuyente(contribuyente_modificado, archivo, arbol);
-                escribir_contribuyente(archivo, contribuyente_modificado, pos);
-            end;
+  arbol_nro_mod := buscar_por_clave(arbol_nro, nro_contribuyente_modificado);
+  pos := arbol_nro_mod.indice;
+  // Buscamos si el numero existe ya en el archivo.
+  While (pos = 0) and (tcl <> -1) do
+    begin
+      Writeln('No existe ningún usuario con este número de contribuyente, que desea hacer?');
+      Writeln('1. Ingresar otro número de contribuyente');
+      Writeln('2. Regresar a la pantalla anterior');
+      Readln(tcl);
+      Case tcl of
+        1: Readln(nro_contribuyente_modificado);
+        2: tcl := -1;
+      end;
+      arbol_nro_mod := buscar_por_clave(arbol_nro, nro_contribuyente_modificado);
+      pos := arbol_nro_mod.indice;
+    end;
+  if tcl <> -1 then
+    begin
+      // Obtenemos el contribuyente viejo, sin modificar.
+      contribuyente_modificado := leer_contribuyente(archivo, pos);
+      // Buscamos las claves.
+      clave_nombre := contribuyente_modificado.apellido + ' ' + contribuyente_modificado.nombre;
+      clave_dni := contribuyente_modificado.dni;
+      // Buscamos los árboles.
+      arbol_nombre_mod := buscar_por_clave(arbol_nombre, clave_nombre);
+      arbol_dni_mod := buscar_por_clave(arbol_dni, clave_dni);
+      // Borramos los árboles.
+      borrar_raiz(arbol_nro_mod);
+      borrar_raiz(arbol_nombre_mod);
+      borrar_raiz(arbol_dni_mod);
+      // Modificamos el contribuyente.
+      modificar_contribuyente(contribuyente_modificado, archivo, arbol);
+      // Guardamos el contribuente.
+      escribir_contribuyente(archivo, contribuyente_modificado, pos);
+      // Obtenemos las nuevas claves.
+      nro_contribuyente_modificado := contribuyente_modificado.numero;
+      clave_nombre := contribuyente_modificado.apellido + ' ' + contribuyente_modificado.nombre;
+      clave_dni := contribuyente_modificado.dni;
+      // Añadimos las nuevas claves a los árboles.
+      sumar_por_nro(arbol_nro, archivo, pos);
+      sumar_por_nombre(arbol_nombre, archivo, pos);
+      sumar_por_dni(arbol_dni, archivo, pos);
+    end;
 
 end;
 
 // Pasar arbol ordenado
-Procedure consulta_contribuyente(var archivo : t_archivo_contribuyentes; var arbol_nombre : t_arbol; var arbol_numero : t_arbol);
+Procedure consulta_contribuyente(var archivo : t_archivo_contribuyentes;
+                                 var arbol_nro : t_arbol;
+                                 var arbol_nombre : t_arbol);
 var
-contribuyente_consultado : t_contribuyente;
-nro_contribuyente_consultado, nombre_contribuyente_consultado : string;
-nombre_consultado, apellido_consultado : string;
-arbol_pos : t_arbol;
-pos, tcl : int16;
+  contribuyente_consultado : t_contribuyente;
+  nro_contribuyente_consultado, nombre_contribuyente_consultado : string;
+  nombre_consultado, apellido_consultado : string;
+  arbol_pos : t_arbol;
+  pos, tcl : int16;
 begin
   Writeln('Cómo desea realizar la consulta?');
   Writeln('1. Por nombre completo');
