d8408bd5686694003b5a1d008bef8edc8f6b044b
Máximo Fernández <maximo03f@gmail.com>
2023-01-10 16:30:24 -0300

    añadido listado de terrenos por zona

--- units/usuario/listados.pas
+++ units/usuario/listados.pas
@@ -4,11 +4,14 @@ unit listados;
 
 {--------------------------------}
 
-uses
+interface
+
+    uses
     terreno in 'units/terreno/terreno.pas';
     lista_terrenos in 'units/terreno/lista_terrenos.pas';
 
-interface
+    // Muestra los terrenos según la zona que le corresponda.
+    procedure terrenos_por_zona(lista : t_lista_terrenos);
 
     // Muestra todas las inscripciones de terrenos dadas en un año.
     procedure inscripciones_anio(var lista : t_lista_terrenos; t_anio : string);
@@ -16,6 +19,38 @@ interface
 {--------------------------------}
 
 implementation
+
+
+    // TODO?: hacer ver como grilla
+    procedure terrenos_por_zona(lista : t_lista_terrenos);
+    var
+        vector_por_zona : t_vector_listas;
+        lista_actual : t_lista_terrenos;
+        terreno_actual : t_terreno;
+        i : 1..5;
+    begin
+        vector_por_zona := generar_vector_por_zona(lista);
+        
+        for i := 1 to 5 do
+        begin
+            writeln('Zona ', i, ':');
+            writeln('');
+
+            lista_actual := vector_por_zona[i];
+            primero_lista_terrenos(lista_actual);
+
+            while not(fin_lista_terrenos(lista_actual)) do
+            begin
+                recuperar_lista_terrenos(lista_actual, terreno_actual);
+                writeln(terreno_actual.domicilio_parcelario);
+                siguiente_lista_terrenos(lista_actual);
+            end;
+
+            writeln('');
+            writeln('Presione una tecla para continuar...');
+            clrscr;
+        end;
+    end;
 
 
     procedure inscripciones_anio(var lista : t_lista_terrenos; t_anio : string);
